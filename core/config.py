b1cf06e861de373396b134dd725c10e2e956374c
alexkhub <aleksandrkhubaev04@gmail.com>
2023-10-09 10:30:08 +0300

    pog

--- core/config.py
+++ core/config.py
@@ -1,3 +1,3 @@
 TOKEN_API = '6454466432:AAF2MyLI751YDtk9LwG9SM3y40FrTdZc6PE'
 ADMIN_TOKEN = 5335815873
-DATABASE = 'D:\\pythonProject\\test\\telegram_bot\\db.sqlite3'
+DATABASE = 'C:\\Users\\3-22\PycharmProjects\\telegram_bot\\db.sqlite3'
--- core/db_commands.py
+++ core/db_commands.py
@@ -2,16 +2,54 @@ import sqlite3 as sql
 
 # from settings import settings
 from config import *
+import asyncio
 
-def get_categorys() -> list:
+
+# def database_connect(func):
+#     def database_script():
+#         connect = sql.connect(DATABASE)
+#         cursor = connect.cursor()
+#         func()
+#         cursor.close()
+#         connect.close()
+#
+#     return database_script()
+
+
+async def get_categorys(command) -> list:
     connect = sql.connect(DATABASE)
 
     cursor = connect.cursor()
-    cursor.execute('SELECT * FROM category ')
-    category = cursor.fetchall()
+    cursor.execute(command)
+    result = cursor.fetchall()
 
     cursor.close()
     connect.close()
-    return category
+    return result
 
-print(get_categorys())
+
+# print(asyncio.run(get_categorys('SELECT * FROM category ')))
+# print(asyncio.run(get_categorys('SELECT * FROM category WHERE category_id=1 ')))
+
+
+class Database():
+    def __init__(self, connect=DATABASE):
+        self.connect = connect
+
+    async def database_functions(self, command):
+        connect = sql.connect(self.connect)
+        cursor = connect.cursor()
+        cursor.execute(command)
+        result = cursor.fetchall()
+        cursor.close()
+        connect.close()
+        return result
+
+    async def new_user(self,  telegram_id):
+         self.database_functions(f'INSERT INTO users (telegram_id) VALUES ({telegram_id});')
+
+
+database = Database()
+print(asyncio.run(database.database_functions(command='SELECT * FROM category WHERE category_id=1 ')))
+
+asyncio.run(database.new_user(12))
