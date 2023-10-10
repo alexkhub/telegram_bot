8c3f9f555c9c1392ff326199c60b728774bd2c64
alexkhub <aleksandrkhubaev04@gmail.com>
2023-10-10 10:43:28 +0300

    pog

--- core/db_commands.py
+++ core/db_commands.py
@@ -5,51 +5,39 @@ from config import *
 import asyncio
 
 
-# def database_connect(func):
-#     def database_script():
-#         connect = sql.connect(DATABASE)
-#         cursor = connect.cursor()
-#         func()
-#         cursor.close()
-#         connect.close()
-#
-#     return database_script()
-
-
-async def get_categorys(command) -> list:
-    connect = sql.connect(DATABASE)
-
-    cursor = connect.cursor()
-    cursor.execute(command)
-    result = cursor.fetchall()
-
-    cursor.close()
-    connect.close()
-    return result
-
-
-# print(asyncio.run(get_categorys('SELECT * FROM category ')))
-# print(asyncio.run(get_categorys('SELECT * FROM category WHERE category_id=1 ')))
-
-
 class Database():
-    def __init__(self, connect=DATABASE):
-        self.connect = connect
+    def __init__(self, link_connect=DATABASE):
+        self.link_connect = link_connect
 
-    async def database_functions(self, command):
-        connect = sql.connect(self.connect)
+    async def get_category(self, category) -> list:
+        connect = sql.connect(self.link_connect)
         cursor = connect.cursor()
-        cursor.execute(command)
+        cursor.execute(f"SELECT * FROM category WHERE category_name='{category}'")
         result = cursor.fetchall()
         cursor.close()
         connect.close()
         return result
 
-    async def new_user(self,  telegram_id):
-         self.database_functions(f'INSERT INTO users (telegram_id) VALUES ({telegram_id});')
+    async def new_user(self, telegram_id) -> None:
+        connect = sql.connect(self.link_connect)
+        cursor = connect.cursor()
+        cursor.execute(f'INSERT INTO users (telegram_id) VALUES ({telegram_id});')
+        connect.commit()
+        cursor.close()
+        connect.close()
+        return None
+
+    async def new_link(self, link_name, link, category) -> None:
+        connect = sql.connect(self.link_connect)
+        cursor = connect.cursor()
+        cursor.execute(f"INSERT INTO links (link_name, link, category) VALUES ('{link_name}', '{link}', {category});")
+        connect.commit()
+        cursor.close()
+        connect.close()
+        return None
 
 
 database = Database()
-print(asyncio.run(database.database_functions(command='SELECT * FROM category WHERE category_id=1 ')))
+asyncio.run(database.new_link(link_name='Документация Django', link='https://django.fun/ru/docs/django/4.2/', category=1))
 
-asyncio.run(database.new_user(12))
+
