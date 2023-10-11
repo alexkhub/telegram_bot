5705150d7e48f6723004a9c78d116cdb544a8a3a
alexkhub <aleksandrkhubaev04@gmail.com>
2023-10-11 14:11:31 +0300

    pog

--- core/models.py
+++ core/models.py
@@ -1,32 +1,52 @@
 from datetime import datetime
+from operator import mod
 
 from peewee import *
 
-db = SqliteDatabase('bot_db.sqlite3')
+db = SqliteDatabase('C:\\Users\\ПК--5\\PycharmProjects\\telegram_bot\\bot_db.sqlite3')
 
-class User(Model):
+
+class Users(Model):
+    user_id = PrimaryKeyField(unique=True)
     user_name = CharField(max_length=40, verbose_name='Имя пользователя')
-    user_url = CharField(max_length=50, verbose_name= 'URL пользователя' , null=True)
+    user_url = CharField(max_length=50, verbose_name='URL пользователя', null=True)
     phone_number = CharField(max_length=14, verbose_name='Номер телефона')
-    admin = BooleanField(default= False, verbose_name='Админ')
-    blocked_user = BooleanField(default= False, verbose_name='Блокировка')
-    created_at = DateField(default=datetime.datetime.now())
+    admin = BooleanField(default=False, verbose_name='Админ')
+    blocked_user = BooleanField(default=False, verbose_name='Блокировка')
+    created_at = DateField(default=datetime.now())
 
     class Meta:
         database = db
+        db_table = 'users'
 
-class Category(Model):
+
+class Categorys(Model):
+    category_id = PrimaryKeyField(unique=True)
     category_name = CharField(max_length=40, verbose_name='Категория')
 
     class Meta:
         database = db
+        db_table = 'categorys'
 
-class Link(Model):
+
+class Links(Model):
+    link_id = PrimaryKeyField(unique=True)
     link_name = CharField(max_length=40, verbose_name='Название статьи')
     link = CharField(max_length=255, verbose_name='Ссылка')
-    category = ForeignKeyField(Category, verbose_name='Категория')
-    user = ForeignKeyField(User, verbose_name='Пользователь')
+    category = ForeignKeyField(Categorys, verbose_name='Категория')
+    user = ForeignKeyField(Users, verbose_name='Пользователь')
 
     class Meta:
         database = db
+        db_table = 'links'
 
+
+# with db:
+#     db.create_tables([Users, Categorys, Links])
+#
+# db.connect()
+# db.commit()
+# db.close()
+
+for user in Users.select():
+    print(user.user_name)
--- main.py
+++ main.py
@@ -44,5 +44,6 @@ async def main() -> None:
 
 
 if __name__ == "__main__":
+
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
     asyncio.run(main())
